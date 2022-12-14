7cf1bd5640bbc05ed3f874574c6ad8dcf69b65ce
Alex223437 <alex223437@gmail.com>
2022-12-14 22:16:26 +0100

    Documentation

--- main.py
+++ main.py
@@ -2,18 +2,66 @@ import math
 import turtle
 
 """
-@authors Ilia Zakharov, Pavel Sakalou, Polina Popova, Aliaksei Sidoryk
+Ilia Zakharov, Pavel Sakalou, Polina Popova, Aliaksei Sidoryk
 """
 
 def start():
-    """Funkce se žádá na uživatele zadat s klávesnici souřadnici bodů vrcholů."""
+    """Funkce se žádá na uživatele zadat s klávesnici souřadnici bodů vrcholů.
+
+        Parametry(Parameters)
+        ---------------------
+        Žadné parametry
+
+        Vrátí(Returns)
+        --------------
+        `x` : Vrátí souřadnice x vrcholy v typu `float`;
+        `y` : Vrátí souřadnice y vrcholy v typu `float`
+
+    """
     x = float(input("Enter x: "))
     y = float(input("Enter y: "))
     return x, y
 
+def seznam_stran(bod1, bod2, bod3):
+    """Tato funkce vypočítá strany trojúhelníka
+
+            Parametry(Parameters)
+            ---------------------
+            `bod1` :
+                Parametr `bod1` je souřadnice `x` a `y` prvního bodu, typ `list`;
+            `bod2` :
+                Parametr `bod2` je souřadnice `x` a `y` druhého bodu, typ `list`;
+            `bod3` :
+                Parametr `bod3` je souřadnice `x` a `y` třetího bodu, typ `list`
+
+            Vrátí(Returns)
+            --------------
+            `list_strana` :
+            Parametr `list_strana` je seznam všech stran trojúhelníka, typ `list`
+
+        """
+    global list_strana
+    list_strana = [((bod2[0] - bod1[0]) ** 2 + (bod2[1] - bod1[1]) ** 2) ** 0.5,
+                   ((bod3[0] - bod2[0]) ** 2 + (bod3[1] - bod2[1]) ** 2) ** 0.5,
+                   ((bod1[0] - bod3[0]) ** 2 + (bod1[1] - bod3[1]) ** 2) ** 0.5]
+
+
 
 def check(sss):
-    """Funkce určuje, zda trojúhelník se zvolenými vrcholí existuje (Součet dvou délek MUSÍ být větší než třetí strana)."""
+    """Funkce určuje, zda trojúhelník se zvolenými vrcholí existuje (Součet dvou délek MUSÍ být větší než třetí strana).
+         Parametry(Parameters)
+        ---------------------
+        `sss` :
+            Parametr `sss` je seznam všech stran trojúhelníka, typ `list`
+
+        Vrátí(Returns)
+        --------------
+        `SystemExit()` :
+            Funkce vrátí `SystemExit()` pokud neexistuje trojúhelníka s uvedenými vrcholy;
+        `True` :
+            Funkce vrátí `True` pokud trojúhelník s uvedenými vrcholy existuje
+
+    """
     if (sss[0] + sss[1]) < sss[2] or (sss[1] + sss[2]) < sss[0] or (sss[2] + sss[0]) < sss[1]:
         raise SystemExit("Your triangle can`t exist")
     else:
@@ -21,17 +69,55 @@ def check(sss):
 
 
 def obvod(sss):
-    """Tato funkce vypočítá obvod trojúhelníku (Součet všech stran ze seznamu stran)"""
+    """Tato funkce vypočítá obvod trojúhelníku (Součet všech stran ze seznamu stran)
+
+        Parametry(Parameters)
+        ---------------------
+        `sss` :
+            Parametr `sss` je seznam všech stran trojúhelníka, typ `list`
+
+        Vrátí(Returns)
+        --------------
+        Součet všech stran ze seznamu stran
+
+    """
     return sss[0] + sss[1] + sss[2]
 
 
 def obsah(sss, per):
-    """Z Heronového vzorce funkce vypočita obsah trojúhelníku"""
+    """Z Heronového vzorce funkce vypočita obsah trojúhelníku
+
+        Parametry(Parameters)
+        ---------------------
+        `sss` :
+            Parametr `sss` je seznam všech stran trojúhelníka, typ `list`;
+        `per` :
+            Parametr `per` je pulka obvodu trojúhelníka, typ `float`
+
+        Vrátí(Returns)
+        --------------
+        Obsah
+
+    """
     return math.sqrt(per*(per-sss[0])*(per-sss[1])*(per-sss[2]))
 
 
 def check_prav(sss):
-    """Funkce určuje, zda trojúhelník je pravoúhlý a oznamuje o tom uživatele"""
+    """Funkce určuje, zda trojúhelník je pravoúhlý a oznamuje o tom uživatele
+
+        Parametry(Parameters)
+        ---------------------
+        `sss` :
+            Parametr `sss` je seznam všech stran trojúhelníka, typ `list`
+
+        Vrátí(Returns)
+        --------------
+        `True` :
+            Funkce vrátí `True` pokud trojúhelník s uvedenými vrcholy je pravoúhlý, typ `boolean`;
+        `False` :
+            Funkce vrátí `False` pokud trojúhelník s uvedenými vrcholy není pravoúhlý, typ `boolean`
+
+    """
     if (sss[0]**2 == sss[1]**2 + sss[2]**2) or (sss[1]**2 == sss[2]**2 + sss[0]**2) or (sss[2]**2 == sss[0]**2 + sss[1]**2):
         print("Trojuhelnik je pravouhly")
         return True
@@ -41,30 +127,40 @@ def check_prav(sss):
 
 
 def uhly(sss):
-    """Tato funkce vypočítá hodnotu úhlů trojúhelníka ve stupních"""
+    """Tato funkce vypočítá hodnotu úhlů trojúhelníka ve stupních
+
+        Parametry(Parameters)
+        ---------------------
+        `sss` :
+            Parametr `sss` je seznam všech stran trojúhelníka, typ `list`
+
+        Vrátí(Returns)
+        --------------
+        `uhel1` :
+            Hodnota prvního úhlu ve stupních, typ `float`;
+        `uhel2` :
+            Hodnota druhého úhlu ve stupních, typ `float`;
+        `uhel3` :
+            Hodnota třetího úhlu ve stupních, typ `float`
+
+    """
     uhel1 = math.degrees(math.acos((sss[0]**2 + sss[2]**2 - sss[1]**2) / (2*sss[0]*sss[2])))
     uhel2 = math.degrees(math.acos((sss[0]**2 + sss[1]**2 - sss[2]**2) / (2*sss[0]*sss[1])))
     uhel3 = math.degrees(math.acos((sss[1]**2 + sss[2]**2 - sss[0]**2) / (2*sss[1]*sss[2])))
     return uhel1, uhel2, uhel3
 
+def trojuhelnik_printer():
+    """Funkce pomoci knihovny Turtle kreslí obrázek trojúhelníku.
 
-if __name__ == '__main__':
-    """Hlavní část programu"""
-    point1 = start()
-    point2 = start()
-    point3 = start()
-    list_strana = [((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2) ** 0.5,
-                   ((point3[0] - point2[0]) ** 2 + (point3[1] - point2[1]) ** 2) ** 0.5,
-                   ((point1[0] - point3[0]) ** 2 + (point1[1] - point3[1]) ** 2) ** 0.5]
-    """Vypočet stran trojúhelníku a vytvoření seznamu"""
-    print("Nasi Strany: "+str(list_strana))
-    check(list_strana)
-    p = obvod(list_strana) / 2
-    print("Obvod je: "+str(obvod(list_strana)))
-    print("Obsah je: "+str(obsah(list_strana, p)))
-    check_prav(list_strana)
-    print(uhly(list_strana))
-    uholky = uhly(list_strana)
+            Parametry(Parameters)
+            ---------------------
+            Žadné parametry
+
+            Vrátí(Returns)
+            --------------
+            Obrazek trojúhelníku
+
+        """
     t = turtle.Turtle()
     t.color('red')
     t.forward(list_strana[0])
@@ -79,3 +175,19 @@ if __name__ == '__main__':
     t.right(180)
     t.left(uholky[0])
     turtle.exitonclick()
+
+
+if __name__ == '__main__':
+    point1 = start()
+    point2 = start()
+    point3 = start()
+    seznam_stran(point1, point2, point3)
+    print("Nasi Strany: "+str(list_strana))
+    check(list_strana)
+    p = obvod(list_strana) / 2
+    print("Obvod je: "+str(obvod(list_strana)))
+    print("Obsah je: "+str(obsah(list_strana, p)))
+    check_prav(list_strana)
+    print(uhly(list_strana))
+    uholky = uhly(list_strana)
+    trojuhelnik_printer()
